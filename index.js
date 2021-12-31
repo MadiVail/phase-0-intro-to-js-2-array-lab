0dcee7f0d7fd83c1bd825062fc333015eda48b03
Madisyn Vail <virtualmadi3@gmail.com>
2021-12-31 13:56:18 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]; 
+
+beforeEach(function (cats) {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+  
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+    
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+
+function appendCat(name) {
+    const newCats = [
+        ...cats.slice(0),
+        name,
+    ];
+    return newCats;
+}
+
+function prependCat(name) {
+    const newCats = [
+        name,
+        ...cats.slice(0),
+    ];
+    return newCats;
+}
+
+function removeLastCat() {
+    const newCats = [
+        ...cats.slice(0, cats.length - 1)
+    ];
+    return newCats;
+}
+
+function removeFirstCat() {
+    const newCats = [
+        ...cats.slice(1)
+    ];
+    return newCats;
+}
